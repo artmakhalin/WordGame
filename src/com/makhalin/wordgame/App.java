6d50b225e85692b71a6c1ae4227cd4a0db5d54c9
artmakhalin <artmakhalin@gmail.com>
2025-03-10 19:08:57 -0400

    setup game loop

---
+++ src/com/makhalin/wordgame/App.java
@@ -0,0 +1,4 @@
+package com.makhalin.wordgame;
+
+public class App {
+}
--- src/com/makhalin/wordgame/WordGuessingGame.java
+++ src/com/makhalin/wordgame/WordGuessingGame.java
@@ -1,4 +1,32 @@
 package com.makhalin.wordgame;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class WordGuessingGame {
+
+    private String wordToGuess;
+    private char[] guessedLetters;
+    private int attempts;
+
+    public WordGuessingGame(String[] wordList, int maxAttempts) {
+        Random rand = new Random();
+        this.wordToGuess = wordList[rand.nextInt(wordList.length)];
+        this.guessedLetters = new char[this.wordToGuess.length()];
+        for (int i = 0; i < this.guessedLetters.length; i++) {
+            this.guessedLetters[i] = '_';
+        }
+        this.attempts = maxAttempts;
+    }
+
+    public void play() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (attempts > 0) {
+            System.out.println("You have " + attempts + " attempts left...");
+            System.out.println("Guess a letter: ");
+
+            attempts--;
+        }
+    }
 }
