c3b57c072c374c542210131e7a21889a9505fd8b
artmakhalin <artmakhalin@gmail.com>
2025-03-10 20:41:30 -0400

    game logic completed

--- src/com/makhalin/wordgame/WordGuessingGame.java
+++ src/com/makhalin/wordgame/WordGuessingGame.java
@@ -20,14 +20,46 @@ public class WordGuessingGame {
     }
 
     public void play() {
-        System.out.println("The word is: " + wordToGuess);
         Scanner scanner = new Scanner(System.in);
+        boolean userHasWon = false;
 
-        while (attempts > 0) {
+        while (attempts > 0 && !userHasWon) {
+            displayState();
             System.out.println("You have " + attempts + " attempts left...");
             System.out.print("Guess a letter: ");
 
-            attempts--;
+            char guess = scanner.nextLine().toLowerCase().charAt(0);
+            if (processGuess(guess)){
+                System.out.println("Correct!");
+                if (new String(guessedLetters).equals(wordToGuess)) {
+                    System.out.println("You have guessed the word! You win!");
+                    displayState();
+                    userHasWon = true;
+                }
+            }
+            else {
+                System.out.println("That guess was incorrect");
+                attempts--;
+            }
         }
+
+        if (!userHasWon) {
+            System.out.println("You're out of guesses! You lose");
+        }
+    }
+
+    private boolean processGuess(char letter) {
+        boolean letterFound = false;
+        for (int i = 0; i < wordToGuess.length(); i++) {
+            if (wordToGuess.charAt(i) == letter) {
+                guessedLetters[i] = letter;
+                letterFound = true;
+            }
+        }
+        return letterFound;
+    }
+
+    private void displayState() {
+        System.out.println("Guessed letters: " + new String(guessedLetters));
     }
 }
